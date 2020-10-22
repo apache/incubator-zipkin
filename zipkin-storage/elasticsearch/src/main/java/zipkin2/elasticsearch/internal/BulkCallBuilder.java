378592c70630d330abb40347f47062eb0ed1169f
minwoox <songmw725@gmail.com>
2020-10-22 11:41:24 +0900

    Update Armera to 1.2.0

--- zipkin-storage/elasticsearch/src/main/java/zipkin2/elasticsearch/internal/BulkCallBuilder.java
+++ zipkin-storage/elasticsearch/src/main/java/zipkin2/elasticsearch/internal/BulkCallBuilder.java
@@ -158,7 +158,9 @@ public final class BulkCallBuilder {
       return writer;
     }
 
-    // Use backpressure, so it's not buffering at all.
+    // There's a high chance that the response is received before the request is complete.
+    // This can be a problem for BulkCallBuilder when it's sending streaming requests.
+    // Hence, we use backpressure, so it's not buffering at all.
     private void writeEntry(HttpRequestWriter writer, int index) {
       if (index == entries.size()) {
         writer.close();
